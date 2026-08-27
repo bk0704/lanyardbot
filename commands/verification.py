6c0908a42b83a79eddf08f3dc61ea766c777d06e
bk0704 <bilal.k1@icloud.com>
2026-08-27 16:55:37 -0400

    feat: created barebones /verify

--- commands/verification.py
+++ commands/verification.py
@@ -5,3 +5,9 @@ class Verification(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    @app_commands.command(name='verify', description='Verify')
+    async def verify(self, interaction):
+        await interaction.response.send_message(f'Verify verify verify')
+
+async def setup(bot):
+    await bot.add_cog(Verification(bot))
