ff3baf71f5e0d5054a6bd552042a22482e58cf57
bk0704 <bilal.k1@icloud.com>
2026-08-27 15:11:19 -0400

    fix: removed message content intent

--- bot.py
+++ bot.py
@@ -3,8 +3,11 @@ from discord.ext import commands
 from discord import app_commands
 import os
 from dotenv import load_dotenv
+from lanyard import Lanyard
 
 # Loading the bot token
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
+bot = Lanyard()
+bot.run(TOKEN)
--- lanyard.py
+++ lanyard.py
@@ -6,7 +6,6 @@ import discord
 class Lanyard(commands.Bot):
     def __init__(self):
         intents = discord.Intents.default()
-        intents.message_content = True
         super().__init__(command_prefix='@', intents=intents)
 
     async def setup_hook(self):
