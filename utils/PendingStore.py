7d1c26b5306dbecbd75715deeae21b7ae2f33fef
bk0704 <bilal.k1@icloud.com>
2026-08-28 12:16:47 -0400

    feat: Created the save method in PendingStore

--- utils/PendingStore.py
+++ utils/PendingStore.py
@@ -4,3 +4,8 @@ class PendingStore:
     def __init__(self, ttl_minutes=15):
         self._entries = {}
         self._ttl = timedelta(minutes=ttl_minutes)
+
+    def save(self, user_id, code, now):
+        self._entries[user_id] = {'code': code,
+                                  'expiry': now + self._ttl}
+
