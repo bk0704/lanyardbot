1b16573b81ca7c8dc91b7f30787622bd2f88b683
bk0704 <bilal.k1@icloud.com>
2026-08-29 19:49:27 -0400

    feat: set verify_button to display EmailModal

--- views/verifyview.py
+++ views/verifyview.py
@@ -1,9 +1,12 @@
 import discord
 
+from modals.emailmodal import EmailModal
+
+
 class VerifyView(discord.ui.View):
     def __init__(self):
         super().__init__(timeout=None)
 
     @discord.ui.button(label='Verify', style=discord.ButtonStyle.success, custom_id='lanyard:verify')
     async def verify_button(self, interaction, button):
-        await interaction.response.send_message('clicked', ephemeral=True)
+        await interaction.response.send_modal(EmailModal)
