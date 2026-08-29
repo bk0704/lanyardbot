aeb16beb734d917ee233e4384275b07e63d520a8
bk0704 <bilal.k1@icloud.com>
2026-08-29 19:45:29 -0400

    feat: created ui.TextInput for email

--- modals/emailmodal.py
+++ modals/emailmodal.py
@@ -1,7 +1,21 @@
+import os
+
+import discord
 from discord import ui
+from dotenv import load_dotenv
+import discord
+
+load_dotenv()
+DOMAIN = os.getenv('ALLOWED_DOMAIN')
 
 class EmailModal(ui.Modal, title='Enter e-mail'):
     def __init__(self, default=None):
         super().__init__()
-        if default: self.email.default = default
+        if default:
+            self.email.default = default
 
+        email = ui.TextInput(label='Enter uni email',
+                             placeholder=f'you{DOMAIN}',
+                             required=True,
+                             max_length=100,
+                             style=discord.TextStyle.short)
