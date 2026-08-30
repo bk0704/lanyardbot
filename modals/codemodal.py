ba747e28fc8b63445d642640791223d30fe0f4ed
bk0704 <bilal.k1@icloud.com>
2026-08-29 21:07:58 -0400

    feat(modal): created the code text input

--- modals/codemodal.py
+++ modals/codemodal.py
@@ -1,4 +1,12 @@
-from discord import ui
+from discord import ui, TextStyle
 
 class CodeModal(ui.Modal, title='Enter OTP'):
+    code = ui.TextInput(label='Enter 6-digit OTP',
+                        placeholder=f'000000',
+                        required=True,
+                        max_length=6,
+                        min_length=6,
+                        style=TextStyle.short
+                        )
+
 
