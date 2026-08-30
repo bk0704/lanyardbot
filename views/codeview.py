4854a44bd3baa54c9e07383d49c33f50f6bf0a32
bk0704 <bilal.k1@icloud.com>
2026-08-29 22:33:13 -0400

    feat(ui): connected CodeModal to CodeView

--- views/codeview.py
+++ views/codeview.py
@@ -1,8 +1,12 @@
 from discord import ui, ButtonStyle
 
+from modals.codemodal import CodeModal
+
+
 class CodeView(ui.View):
     def __init__(self):
         super().__init__(timeout=900)
 
     @ui.button(label='Enter OTP', style=ButtonStyle.primary)
     async def code_button(self, interaction, button):
+        await interaction.response.send_modal(CodeModal())
